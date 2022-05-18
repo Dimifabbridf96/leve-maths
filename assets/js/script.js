938989dc11b478481c4ebd17fbf7f792e0c8cfd3
Dimifabbridf96 <dimi.fabbri@gmail.com>
2022-05-18 17:37:17 +0000

    add first game with checked answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,30 +5,59 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let button of buttons){
         button.addEventListener("click", function(){
             if(this.getAttribute("data-type")=== "submit"){
-                alert("You clicked Submit!");
+                checkAnswer();
             } else{
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+    runGame('addition');
 });
+
 /**
  * The main ga 'loop', called when the script is first loaded and after the user's answer has been processed 
  */
-function runGame(){
+function runGame(gameType){
 let num1 = Math.floor(Math.random()*25) + 1;
 let num2 = Math.floor(Math.random()*25) + 1;
+
+if(gameType === 'addition'){
+    displayAdditionQuestion(num1,num2);
+}
+else{
+alert(`Unknown game type: ${gameType}`);
+throw `Unknown game type: ${gameType}. Aborting!`;
 }
 
-runGame()
+}
+
+
 function checkAnswer(){
 
+let userAnswer = parseInt(document.getElementById('answer-box').value);
+let calculatedAnswer = calculateCorrectAnswer();
+let isCorrect = userAnswer === calculatedAnswer[0];
+if (isCorrect){
+    alert('Hey! You got it right! :D')
+}else{
+    alert(`Your answer was ${userAnswer} The answer was ${calculatedAnswer[0]}!`);
+}
+runGame(calculateCorrectAnswer[1]);
 
 }
 function calculateCorrectAnswer(){
 
+let operand1 = parseInt(document.getElementById('operand1').innerText);
+let operand2 = parseInt(document.getElementById('operand2').innerText);
+let operator = document.getElementById('operator').innerText;
 
+if (operator === '+'){
+    return[operand1 + operand2, 'addition'];
+}else{
+    alert(`Unimplemented operator ${operator}`);
+    throw `Unimplemented operator ${operator}. Aborting!`;
+}
 }
 function incrementScore(){
 
@@ -38,15 +67,20 @@ function incrementWrongAnswer(){
 
 
 }
-function dispalyAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+document.getElementById('operand1').textContent = operand1;
+document.getElementById('operand2').textContent = operand2;
+document.getElementById('operator').textContent = '+';
+}
+function displaySubtractQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '-';
 
 }
-function displaySubtractQuestion(){
-
-
-}
-function displayMultiplyQuestion(){
-
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementsByTagName('operand1').textContent = operand1;
+    document.getElementsByTagName('operand2').textContent = operand2;
+    document.getElementsByTagName('operator').textContent = 'x';
 
 }
